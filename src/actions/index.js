d30393754b5055866b0fb3244d915b6a73c0dfa1
Grisel Matos <griselmatosm@gmail.com>
2020-05-26 18:04:21 +0200

    Using redux-thunk to fetch data from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS',
-  };
+import getData from '../services/getDataFromApi';
+
+//al usar redux-thunk en action creators asíncronos la función del fetch retorna otra función que recibe
+//como parámatros dispatch y getState (en este caso no necesitamos el estado)
+export const fetchPosts = () => async (dispatch) => {
+  const response = await getData.get('/posts');
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
