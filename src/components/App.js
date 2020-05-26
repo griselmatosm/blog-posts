4c10f1976267c9976be43c7ef92c99f347200fcd
Grisel Matos <griselmatosm@gmail.com>
2020-05-26 14:48:29 +0200

    Add PostList component. Add action cretor to fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 function App() {
   return (
     <div className="ui container">
-hello world
+      <PostList />
     </div>
   );
 }
