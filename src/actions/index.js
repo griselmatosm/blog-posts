df062efe9d6e4952e0263cd26624cc95957148bb
Grisel Matos <griselmatosm@gmail.com>
2020-05-27 12:50:12 +0200

    Add UserHeader component and connect with userReducer reducer. Render and paint user name in every single post

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,6 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 export const fetchUser = (id) => async (dispatch) => {
-  const response = await getData.get(`/user/${id}`);
+  const response = await getData.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
